44f7c99c895a1d99150e3d6a55e8bd65be818e50
ChrisZZHong <zz397@georgetown.edu>
2022-06-14 23:19:33 -0400

    update

--- src/db/mysql/MySQLDBUtil.java
+++ src/db/mysql/MySQLDBUtil.java
@@ -4,8 +4,8 @@ public class MySQLDBUtil {
 	private static final String HOSTNAME = "localhost";
 	private static final String PORT_NUM = "3306"; // change it to your mysql port number
 	public static final String DB_NAME = "laiproject";
-	private static final String USERNAME = "debian-sys-maint";
-	private static final String PASSWORD = "8YQn7DrJ2cMRl2MP";
+	private static final String USERNAME = "root";
+	private static final String PASSWORD = "root";
 	public static final String URL = "jdbc:mysql://" + HOSTNAME + ":" + PORT_NUM + "/" + DB_NAME + "?user=" + USERNAME
 			+ "&password=" + PASSWORD + "&autoReconnect=true&serverTimezone=UTC";
 }
